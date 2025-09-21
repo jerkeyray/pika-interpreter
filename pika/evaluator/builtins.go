620ddb2da02eea1d3f9183bd422c6d873696ee1f
srivastavya02 <srivastavya24@gmail.com>
2025-09-21 20:03:26 +0530

    feat: add len, rest and push function to arrays

--- pika/evaluator/builtins.go
+++ pika/evaluator/builtins.go
@@ -4,13 +4,19 @@ import (
 	"pika/object"
 )
 
+// map for string names to actual functions
 var builtins = map[string]*object.Builtin{
+	// map "len" to new builtin function object
 	"len": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
+			// check number of arguments
 			if len(args) != 1 {
 				return newError("wrong number of arguments. got=%d, want=1", len(args))
 			}
+			// check argument type
 			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
@@ -18,4 +24,85 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	}, 
+
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			// check number of arguments
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			// check argument type
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+			// return first element of array else null if empty
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+			return NULL
+		},
+	},
+
+	"last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			// check number of arguments
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			// check argument type
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `last` must be ARRAY, got %s", args[0].Type())
+			}
+			// return last element of array else null if empty
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[len(arr.Elements)-1]
+			}
+			return NULL
+		},
+	},
+
+	"rest": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			// check number of arguments
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			// check argument type
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+			// return array containing all elements except first else null if empty
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length)
+				copy(newElements, arr.Elements[1: length]) // copy all elements except first
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
+
+	"push": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			// check number of arguments
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			// check first argument type
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("first argument to `push` must be ARRAY, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]object.Object, length+1)
+			copy(newElements, arr.Elements) // copy all elements of original array
+			newElements[length] = args[1]   // add new element at the end
+
+			return &object.Array{Elements: newElements}
+		},
+	},
 }
